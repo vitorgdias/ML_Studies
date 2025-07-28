d3a35c73cc13b240bc5e6faad16e0ffcc322b04f
vitorgdias <vitorgalves@live.com>
2025-07-28 17:28:44 -0300

    Add data processing and Decision Tree model for happiness prediction

---
+++ metrics.py
@@ -0,0 +1,72 @@
+# %%
+
+import pandas as pd
+
+df = pd.read_csv('./data/comunidade_dados.csv')
+df.head()
+
+# %%
+df = df.replace({'Sim': 1, 'Não': 0})
+df.head()
+
+# %%
+
+num_vars = [
+    "Curte games?",
+    "Curte futebol?",
+    "Curte livros?",
+    "Curte jogos de tabuleiro?",
+    "Curte jogos de fórmula 1?",
+    "Curte jogos de MMA?",
+    "Idade",
+
+]
+dummy_vars = [
+    "Como conheceu o Téo Me Why?",
+    "Quantos cursos acompanhou do Téo Me Why?",
+    "Estado que mora atualmente",
+    "Área de Formação",
+    "Tempo que atua na área de dados",
+    "Posição da cadeira (senioridade)"
+]
+
+df_analysis = pd.get_dummies(df[dummy_vars]).astype(int)
+
+df_analysis[num_vars] = df[num_vars].copy()
+
+# %%
+df_analysis['pessoa feliz?'] = df['Você se considera uma pessoa feliz?']
+
+df_analysis
+
+# %%
+from sklearn import tree
+
+features = df_analysis.columns[:-1].to_list()
+
+X = df_analysis[features]
+y = df_analysis['pessoa feliz?']
+
+arvore = tree.DecisionTreeClassifier(random_state=42, min_samples_leaf=5)
+
+arvore.fit(X,y)
+
+# %%
+
+arvore_predict = arvore.predict(X)
+arvore_predict
+
+df_predict = df_analysis[['pessoa feliz?']]
+df_predict['predict_arvore'] = arvore_predict
+
+df_predict
+
+# %%
+
+# Checking the df differences using mean
+(df_predict['pessoa feliz?'] == df_predict['predict_arvore']).mean()
+
+# %%
+
+# Confusion Matrix
+pd.crosstab(df_predict['pessoa feliz?'], df_predict['predict_arvore'])
